241c8f7da2ab7da4212a1965399ead047608853a
Ilsur <hooleangalaxy@gmail.com>
2024-02-29 18:07:39 +0500

    2

--- interface.py
+++ interface.py
@@ -1,67 +1,41 @@
-# from PyQt5.QtWidgets import * 
-# from PyQt5.QtCore import Qt
-# import nltk_studies
-
-
-
-# def main():
-#     app = QApplication([])
-#     window = QWidget()
-#     window.setGeometry(0, 0, 600, 600)
-#     window.setWindowTitle("My project")
-
-#     layout = QVBoxLayout()
-  
-#     label = QLabel("Drag a file or fill in the box")
-#     button = QPushButton("button")
-#     button.clicked.connect(on_clicked)
-
-#     layout.addWidget(label)
-#     layout.addWidget(button)
-
-
-#     window.setLayout(layout)
-
-#     window.show()
-#     app.exec_()
-
-
-# def on_clicked():
-#     message = QMessageBox()
-#     message.setText("clicked")
-#     message.exec_()
-
-
-# if __name__  == '__main__':
-#     main()
-import nltk_studies as ns
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QFileDialog
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QDragEnterEvent, QDropEvent
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QFileDialog
+from PyQt6.QtCore import Qt, QMimeData
+from PyQt6.QtGui import QDragEnterEvent, QDropEvent, QIcon
 
-class FileSelectWindow(QWidget):
+class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("File Selector")
-        self.setGeometry(100, 100, 400, 200)
+        self.setWindowTitle("NLTK")
+        self.setWindowIcon(QIcon("icons/python.ico"))
+
+        mainStyleSheet = (
+            "background-color: #585858"
+        )
+
+        self.setStyleSheet(mainStyleSheet)
+        self.resize(1000, 800)
+        self.center()
 
         layout = QVBoxLayout()
 
         self.label = QLabel("Drop file here or click to select")
-        self.label.setAlignment(Qt.AlignCenter)
+        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         layout.addWidget(self.label)
 
         self.button = QPushButton("Select File")
         self.button.clicked.connect(self.selectFile)
         layout.addWidget(self.button)
 
+        self.setAcceptDrops(True)
+
         self.setLayout(layout)
 
-    def open_AnalysisWindow(self, filename):
-        self.analysis_window = AnalysisWindow(filename)
-        self.analysis_window.show()
+    def center(self):
+        screen_geometry = QApplication.primaryScreen().geometry()
+        center_point = screen_geometry.center()
+        self.move(center_point - self.rect().center())
 
     def dragEnterEvent(self, event: QDragEnterEvent):
         if event.mimeData().hasUrls():
@@ -72,33 +46,21 @@ class FileSelectWindow(QWidget):
     def dropEvent(self, event: QDropEvent):
         files = [url.toLocalFile() for url in event.mimeData().urls()]
         if files:
-            self.label.setText(files[0])
+            print("File dropped:", files[0])
 
     def selectFile(self):
         filename, _ = QFileDialog.getOpenFileName(self, "Select File")
         if filename:
-            self.open_AnalysisWindow(filename)
-            
+            print("File selected:", filename)
 
     
-            
 
-class AnalysisWindow(QWidget):
-    def __init__(self, filename):
-        super().__init__()
-
-        layout = QVBoxLayout()
-
-        self.label = QLabel(ns.read_file(filename))
-        layout.addWidget(self.label)
-
-        self.setLayout(layout)
 
 def main():
     app = QApplication(sys.argv)
-    window = FileSelectWindow()
+    window = MainWindow()
     window.show()
-    app.exec_()
+    sys.exit(app.exec())
 
 if __name__ == "__main__":
     main()
--- nltk_studies.py
+++ nltk_studies.py
@@ -1,66 +1,93 @@
-import nltk 
-nltk.download('punkt')
+import nltk
 import string
 
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
+from nltk.collocations import BigramAssocMeasures, BigramCollocationFinder
 
+from nltk.corpus import stopwords
 from nltk.tokenize import sent_tokenize, word_tokenize
 
+"""
+This code snippet defines several functions related to text analysis using the NLTK library. The functions include:
 
-def read_file(filename):
-    with open (filename, "r", encoding = "utf-8") as file:
-        text = file.read()        
-        # if param == "word":
-        #     res = word_tokenize(text)
-        # elif param == "sent":
-        #     res = sent_tokenize(text)
-        # elif param == "raw":
-        #     res = text
-        # else:
-        #     res = text
+- read_file: Reads a file and returns the raw text, word tokens, and sentence tokens.
+- max_length: Finds the word with the maximum length in a list of word tokens.
+- unique_words: Removes stop words from a list of word tokens and returns the unique words and the number of unique words.
+- token_number: Returns the number of tokens in a given string.
+- average_words_length: Calculates the average length of words in a list of word tokens.
+- average_sent_length: Calculates the average length of sentences in a list of sentence tokens.
+- average_words_sent: Calculates the average number of words per sentence in a list of word tokens and sentence tokens.
 
+Note: The code snippet also imports necessary modules and downloads the required NLTK resources.
+"""
 
-        # max_length = 0
-        # max_length_word =""
+def read_file(filename, remove_punctuation=True):
+    with open(filename, "r", encoding="utf-8") as file:
+        text = file.read()
+
+        if remove_punctuation:
+            punctuation = string.punctuation + "”“’»«—"
+            translator = str.maketrans('', '', punctuation)
+            raw_text = text.translate(translator).lower()
+
         
-        # for word in res:
-        #     if len(word) > max_length:
-        #         max_length = len(word)
-        #         max_length_word = word
+        stop_words = set(stopwords.words(["english", "russian"]))
+        word_tokens = [word for word in word_tokenize(raw_text) if word not in stop_words and word.isalpha()]
+        sent_tokens = sent_tokenize(raw_text)
 
-        # punct_remove = str.maketrans('', '', string.punctuation)
-        # res = [w.translate(punct_remove) for w in res]
-        # res = [w.lower() for w in res]
+        return raw_text, word_tokens, sent_tokens
+
+def max_length(word_tokens):
+    max_length = 0
+    max_length_word = ""
+
+    for word in word_tokens:
+        if len(word) > max_length:
+            max_length = len(word)
+            max_length_word = word
+
+    return max_length, max_length_word
+
+def unique_words(word_tokens):
+
+    unique_words = set(word_tokens)
+    unique_words_number = len(word_tokens)
+
+    return unique_words, unique_words_number
+
+def token_number(s):
+    return len(s)
+
+def average_words_length(word_tokens, raw_text):
+    return round(len(raw_text) / len(word_tokens), 2)
+
+def average_sent_length(sent_tokens, raw_text):
+    return round(len(raw_text) / len(sent_tokens), 2)
+
+def average_words_sent(word_tokens, sent_tokens):
+    return round(len(word_tokens) / len(sent_tokens), 2)
+
+
+def word_cloud(raw_text):
+    wordcloud = WordCloud(width=800, height=400, background_color="white").generate(raw_text)
+    plt.figure(figsize=(10, 5))
+    plt.imshow(wordcloud, interpolation="bilinear")
+    plt.axis("off")
+    plt.show()
+
+
+def collocations(word_tokens, num=20):
+    finder = BigramCollocationFinder.from_words(word_tokens)
+    collocations_counts = {}
+    for bigram, freq in finder.ngram_fd.items():
+        collocations_counts[bigram] = freq
     
-        # if measure is not None:
-        #     if measure == "length":
-        #         return len(res)
-            
-        #     elif measure == "unique":
-        #         return len(set(res))
-            
-        #     elif measure == "max_length_word":
-        #         return max_length_word
-            
-        #     elif param2 is not None and measure == "compare":
-        #         if param2 == "word":
-        #             res2 = word_tokenize(text)
-
-        #         elif param2 == "sent":
-        #             res2 = sent_tokenize(text)
-
-        #         else:
-        #             raise ValueError("Неверный параметр 3")
-        #         res2 = [w.translate(punct_remove) for w in res2]
-        #         res2 = [w.lower() for w in res2]
-
-        #         return round(len(res) / len(res2), 2)
-
-        #     else: 
-        #         raise ValueError("Неверный параметр 4")
-
-
-        # return res
-        return text
-
+    sorted_collocations = dict(sorted(collocations_counts.items(), key=lambda item: item[1], reverse=True))
+    for i, (bigram, freq) in enumerate(sorted_collocations.items()):
+        if i < num:
+            print(bigram, freq)
+        else:
+            break
 
